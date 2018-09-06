570808098cca582a2a947be79125e883c3bb0acb
wangui.njoka <wangui.njoka@gmail.com>
2018-09-06 19:28:57 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -1,11 +1,18 @@
 from app import create_app,db
 from app.models import User
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
+
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
 manager.add_command('server',Server)
 
 manager.add_command('server',Server)
