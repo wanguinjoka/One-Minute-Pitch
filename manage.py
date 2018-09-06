99fecd8bae1d42df2a98c86d837c7a15efa29452
wangui.njoka <wangui.njoka@gmail.com>
2018-09-06 18:59:01 +0300

    install alchemy that enable flask-migration

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
-from app import create_app
+from app import create_app,db
+from app.models import User
 from flask_script import Manager,Server
 
 # Creating app instance
@@ -9,6 +10,9 @@ manager.add_command('server',Server)
 
 manager.add_command('server',Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 
 if __name__ == '__main__':
     manager.run()
