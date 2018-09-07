b0185a705986b74903b10250083ddeb02d5fc122
wangui.njoka <wangui.njoka@gmail.com>
2018-09-07 17:34:01 +0300

    profile displaying

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
+from ..models import User
 from flask_login import login_required
 
 # Views
@@ -10,3 +11,12 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
--- app/models.py
+++ app/models.py
@@ -8,21 +8,23 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
-    __tablename__ ='users'
+    __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
     password_hash = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
         
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
-        def verify_password(self,password):
-                    return check_password_hash(self.pass_secure,password)
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f'User {self.username}'
